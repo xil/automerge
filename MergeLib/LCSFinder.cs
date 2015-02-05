f59bafac43f3d4859a619dbc0eca29b8260c4f44
dpaskov <dpaskov@biosecure.ru>
2015-02-05 17:18:38 +0400

    Longest common subsequence algorithm is working but not tested properly

--- MergeLib/LCSFinder.cs
+++ MergeLib/LCSFinder.cs
@@ -6,71 +6,55 @@ using System.Threading.Tasks;
 
 namespace MergeLib
 {
-    class LCSFinder
+    internal class LCSFinder
     {
         public LCSFinder()
         {
 
         }
 
-        /*function LCSLength(X[1..m], Y[1..n])
-    C = array(0..m, 0..n)
-    for i := 0..m
-       C[i,0] = 0
-    for j := 0..n
-       C[0,j] = 0
-    for i := 1..m
-        for j := 1..n
-            if X[i] = Y[j]
-                C[i,j] := C[i-1,j-1] + 1
+        public List<string> findLCS(List<string> fileA, List<string> fileO)
+        {
+            int[,] C = LCSLength(fileA, fileO);
+            return backtrack(ref C, ref fileA, ref fileO, fileA.Count, fileO.Count);
+        }
+
+        int[,] LCSLength(List<string> fileA, List<string> fileO)
+        {
+            int m = fileA.Count+1;
+            int n = fileO.Count+1;
+
+            int[,] C = new int[m, n];
+            for (int i = 0; i < m; i++)
+                C[i, 0] = 0;
+            for (int j = 0; j < n; j++)
+                C[0, j] = 0;
+            for (int i = 1; i < m; i++)
+                for (int j = 1; j < n; j++)
+                    if (fileA[i-1].Equals(fileO[j-1]))
+                        C[i, j] = C[i - 1, j - 1] + 1;
+                    else
+                        C[i,j]=Math.Max(C[i,j-1],C[i-1,j]);
+            return C;
+        }
+
+        List<string> backtrack(ref int[,] C, ref List<string> fileA, ref List<string> fileO, int i, int j)
+        {
+            if (i == 0 || j == 0)
+                return new List<string>();
+            else if (!fileA[i - 1].Equals(fileO[j - 1]))
+            {
+                if (C[i, j - 1] > C[i - 1, j])
+                    return backtrack(ref C, ref fileA, ref fileO, i, j - 1);
+                else
+                    return backtrack(ref C, ref fileA, ref fileO, i - 1, j);
+            }
             else
-                C[i,j] := max(C[i,j-1], C[i-1,j])
-    return C[m,n]*/
-
-
-
-        /*function backtrackAll(C[0..m,0..n], X[1..m], Y[1..n], i, j)
-    if i = 0 or j = 0
-        return {""}
-    else if X[i] = Y[j]
-        return {Z + X[i] for all Z in backtrackAll(C, X, Y, i-1, j-1)}
-    else
-        R := {}
-        if C[i,j-1] ≥ C[i-1,j]
-            R := backtrackAll(C, X, Y, i, j-1)
-        if C[i-1,j] ≥ C[i,j-1]
-            R := R ∪ backtrackAll(C, X, Y, i-1, j)
-        return R*/
-
-
-
-        /*function backtrack(C[0..m,0..n], X[1..m], Y[1..n], i, j)
-    if i = 0 or j = 0
-        return ""
-    else if  X[i] = Y[j]
-        return backtrack(C, X, Y, i-1, j-1) + X[i]
-    else
-        if C[i,j-1] > C[i-1,j]
-            return backtrack(C, X, Y, i, j-1)
-        else
-            return backtrack(C, X, Y, i-1, j)*/
-
-
-
-        /*function printDiff(C[0..m,0..n], X[1..m], Y[1..n], i, j)
-    if i > 0 and j > 0 and X[i] = Y[j]
-        printDiff(C, X, Y, i-1, j-1)
-        print "  " + X[i]
-    else if j > 0 and (i = 0 or C[i,j-1] ≥ C[i-1,j])
-        printDiff(C, X, Y, i, j-1)
-        print "+ " + Y[j]
-    else if i > 0 and (j = 0 or C[i,j-1] < C[i-1,j])
-        printDiff(C, X, Y, i-1, j)
-        print "- " + X[i]
-    else
-        print ""*/
-
-
-       
+            {
+                List<string> result = backtrack(ref C, ref fileA, ref fileO, i - 1, j - 1);
+                result.Add(fileA[i - 1]);
+                return result;
+            }
+        }
     }
 }
---
+++ MergeTest/LCSTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using MergeLib;
+
+namespace MergeLibTest
+{
+    [TestClass]
+    public class LCSTest
+    {
+        [TestMethod]
+        public void LCS_first_Test()
+        {
+            List<string> O = new List<string>(new string[5] { "1", "2", "3", "4", "10" });
+            List<string> A = new List<string>(new string[11] { "0", "1", "1.5", "2", "3", "4", "4.5", "5", "10", "11", "12" });
+            List<string> B = new List<string>(new string[10] { "1", "4", "4.1", "4.2", "4.3", "4.4", "5", "8", "9", "10" });
+            /*List<string> expectation = new List<string>(new string[27] { "0", "1", 
+                                          "================================ Overlapping ================================",
+			                              "================================    FileA    ================================",
+                                          "1.5", "2", "3", 
+                                          "================================    FileO    ================================",
+                                          "2", "3", 
+                                          "================================     End     ================================",
+                                          "4",
+                                          "================================ Overlapping ================================",
+			                              "================================    FileA    ================================",
+                                          "4.5",
+                                          "================================    FileB    ================================",
+                                          "4.1", "4.2", "4.3", "4.4", 
+                                          "================================     End     ================================",
+                                          "5","8","9","10","11","12"});
+             */
+            List<string> R;
+
+            LCSFinder finder = new LCSFinder();
+            R = finder.findLCS(A, O);
+
+            //CollectionAssert.AreEqual(expectation, R);
+        }
+    }
+}
